7098501dbc71f87ae2ec11dc1b4316f3544b6781
MLucasDelgado <lucasdelgado1011@gmail.com>
2025-06-13 13:19:27 -0300

    Creacion del archivo csv

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -30,18 +30,31 @@ else
 }
 
 string[] archivos = Directory.GetFiles(path);
+string rutaReporte = Path.Combine(path, "reporte_archivos.csv");
+
 if (archivos.Length > 0)
 {
     Console.WriteLine("\nArchivos encontrados:");
+
+    var writer = new StreamWriter(rutaReporte); // crea un archivo nuevo o sobrescribe uno existente
+    writer.WriteLine("Nombre del Archivo; Tamaño (KB); Fecha de Última Modificación");
+
     foreach (var archivo in archivos)
     {
         var nombreArchivo = Path.GetFileName(archivo);
         FileInfo fileInfo = new FileInfo(archivo);
-        long tamañoKb = fileInfo.Length / 1024;
+        double tamañoKb = Math.Round(fileInfo.Length / 1024.0, 2);
+        string fechaModificacion = fileInfo.LastWriteTime.ToString("yyyy-MM-dd HH:mm:ss");
         Console.WriteLine($"- {nombreArchivo} ({tamañoKb} KB)");
+
+        string lineaCsv = $"{nombreArchivo}; {tamañoKb}; {fechaModificacion}";
+        writer.WriteLine(lineaCsv);
     }
+
+    writer.Close(); // para asegurar de que se liberen los recursos y se guarde correctamente el archivo
 }
 else
 {
     Console.WriteLine("No se encontraron archivos");
 }
+Console.WriteLine($"\nArchivo de reporte generado: {rutaReporte}");
